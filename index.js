7449e9847373cc145f123bc52b20c247a2e0071c
Sebastian <jmedinaca02@uninpahu.edu.co>
2021-03-17 19:42:31 -0500

    express

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+//var expressWs = require('express-ws')(app);
+ 
+/*app.use(function (req, res, next) {
+  console.log('middleware');
+  req.testing = 'testing';
+  return next();
+});*/
+ 
+app.get('/', function(req, res, next){
+  console.log('get route', req.testing);
+  res.send('cargado')
+  res.end();
+});
+app.get('/primero', function(req, res, next){
+  console.log('get route', req.testing);
+  res.send('primer endpoint')
+  res.end();
+});
+ 
+/*app.ws('/', function(ws, req) {
+  ws.on('message', function(msg) {
+    console.log(msg);
+  });
+  console.log('socket', req.testing);
+});*/
+ 
+app.listen(3000);
