debb50af7c60322266e2cbb931af5ef6ea4c215a
Sebastian <jmedinaca02@uninpahu.edu.co>
2021-04-13 08:22:26 -0500

    conexionBD

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
 const express = require('express')
 const app = express()
+const bodyParser= require('body-parser')
+const mongoose= require('mongoose')
+const ProductSchema= require('./schemas/schema_mongoose')
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 
-app.get('/Inicio',(req,res)=>{
-  res.send('Inicio');
-  res.render('/scr/index.html');
+
+app.get('/api/inicio',(req,res)=>{
+  res.send('Inicio');  
 })
 
-app.get('/SignUp',(req,res)=>{
+app.post('api/signUp',(req,res)=>{
   res.send('Inicio sesion');
 })
 
-app.get('/SignIn',(req,res)=>{
+app.post('api/signIn',(req,res)=>{
   res.send('Registrarse');
 })
-app.post('/Cambio',(req,res)=>{
-  res.send('Cambiado')
+app.get('api/item',(req,res)=>{
+      res.send('Producto');
+    })
+app.get('/api/item/:productId',(req,res)=>{
+
 })
-app.delete('/Borrado',(req,res)=>{
+app.post('api/addProduct',(req,res)=>{
+  console.log('POST /api/product')
+  console.log(req.body)
+
+
+  let producto= new ProductSchema()
+  producto.name=req .body.name
+  producto.picture=req.body.picture
+  producto.price=req.body.price
+  producto.category= req.body.price
+  producto.description=req.body.description
+  
+})
+app.delete('api/borrado',(req,res)=>{
   res.send('Borrado');
 })
+
+//mongoose.connect('mongodb://localhost:27017/shop',{useNewUrlParser: true, useUnifiedTopology: true},(err,res)=>{
+//  if(err) throw err;
+//  console.log('se conecto')
+//})
 app.listen(3000, ()=>{
   console.log("Server in port 3000")
+  
 })
