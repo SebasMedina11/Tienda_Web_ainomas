e1a562f194ae72ff26766b8bb531db0d6c39d704
Sebastian <jmedinaca02@uninpahu.edu.co>
2021-04-14 09:57:53 -0500

    Conexion base de datos y endpoints

--- index.js
+++ index.js
@@ -1,42 +1,51 @@
 const express = require('express')
 const app = express()
+const path= require('path')
 const bodyParser= require('body-parser')
 const mongoose= require('mongoose')
 const ProductSchema= require('./schemas/schema_mongoose')
+//settings
+
+
+//middlewares
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
-
+//routes
 app.get('/api/inicio',(req,res)=>{
   res.send('Inicio');  
 })
 
-app.post('api/signUp',(req,res)=>{
+app.post('/api/signUp',(req,res)=>{
   res.send('Inicio sesion');
 })
 
-app.post('api/signIn',(req,res)=>{
+app.get('/api/signIn',(req,res)=>{
   res.send('Registrarse');
 })
-app.get('api/item',(req,res)=>{
+app.get('/api/item',(req,res)=>{
  })
 app.get('/api/item/:productId',(req,res)=>{
 
 })
-app.post('api/addProduct',(req,res)=>{
-  console.log('POST /api/product')
+app.post('/api/addProduct',(req,res)=>{
+
   console.log(req.body)
-
-
   let producto= new ProductSchema()
-  producto.name=req .body.name
+  producto.name=req.body.name
   producto.picture=req.body.picture
   producto.price=req.body.price
-  producto.category= req.body.price
+  producto.category= req.body.category
   producto.description=req.body.description
+
+  producto.save((err, productStored)=>{
+    if (err) res.status(500).send({message:"Error al salvar el producto"})
+
+    res.status(200).send({product: productStored})
+  })
   
 })
-app.delete('api/borrado',(req,res)=>{
+app.delete('/api/borrado',(req,res)=>{
   res.send('Borrado');
 })
 
--- schemas/schema_mongoose.js
+++ schemas/schema_mongoose.js
@@ -11,4 +11,4 @@ const ProductSchema = Schema({
 
 })
 
-mongoose.model('Product',ProductSchema)
+module.exports=mongoose.model('Product',ProductSchema)
